ef48a4fbb4d095e5aa71a47dce530af5a465f835
Jose Olguin Lagos <hellocodefusion@gmail.com>
2026-06-18 15:48:44 -0400

    hotfix import

--- server/core/urls.py
+++ server/core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.shortcuts import RedirectView
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
